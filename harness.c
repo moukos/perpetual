53de7a30e618cc95100a2d47e8e5aeb499671688
Themis Melissaris <melissth@gmail.com>
2019-03-05 02:17:36 -0500

    Adding argc/argv

--- harness.c
+++ harness.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 // Must compile with -lpthread
 // Will generate warnings
 extern void P1(int * address);
 extern void P2(int * address);
-int n=2;
+int n=0;
 
-int main(argc,argv) {
+/*
+ * TODO: 1) force threads to run on different cores
+ * 	 2) specify memory locations of Litmus tests a) as global variables ?
+ * 	 	b) beginning of memory array ?, c) part of struct?, d) other?
+ */
+int main(int argc,char *argv[]) {
 
     int i=0;
-    int *x=(int*) malloc(n*sizeof(int));  
-    int *y=(int*) malloc(n*sizeof(int));
+    
+    if(argc >= 2)
+    {
+	int input = atoi(argv[1]);
+	n = input;
+    	int *x = (int*) malloc(n*sizeof(int));  
+    	int *y = (int*) malloc(n*sizeof(int));
 
-    pthread_t thread1;
-    pthread_t thread2;
-    pthread_create(&thread1, NULL, P1, x);
-    pthread_create(&thread2, NULL, P2, y);
+    	pthread_t thread1;
+    	pthread_t thread2;
+    	pthread_create(&thread1, NULL, P1, x);
+    	pthread_create(&thread2, NULL, P2, y);
 
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-    for(i=0;i<n;i++){
-	printf("x: %d, y: %d\n", x[i],y[i]);
+    	pthread_join(thread1, NULL);
+    	pthread_join(thread2, NULL);
+    	for(i=0;i<n;i++){
+		printf("x: %d, y: %d\n", x[i],y[i]);
+    	}
     }
-
+    else
+    {
+	printf("Insufficient number of arguments. Specify # of iterations\n");
+    }
     return 1;
 }
 

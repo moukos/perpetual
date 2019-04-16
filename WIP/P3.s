b4ad2f3205d26bc483475ba7bbaf747960ba889b
Themis Melissaris <melissth@gmail.com>
2019-04-16 16:02:20 -0400

    Work In Progress

---
+++ WIP/P3.s
@@ -0,0 +1,39 @@
+	.text
+	.globl P2
+	.globl x
+	.globl y
+	.type P2, @function
+
+P2:
+	pushq %rbp
+	movq %rsp, %rbp
+	movq %rdi, -24(%rbp) # Copies address of array
+	movl $0, -4(%rbp) # Sets loop counter to 0
+	jmp .L2
+.L3:
+#	movl -4(%rbp), %eax #copy counter to eax
+#	cltq	# extend eax to rax
+#	leaq 0(,%rax,4), %rdx # based on counter, load array offset into rdx
+	movq -24(%rbp), %rax #move array address to rax
+	movq 16(%rax), %rax
+	movl -4(%rbp), %edx
+	salq $2, %rdx
+        addq %rdx, %rax
+        movl $15, (%rax)
+#	addq %rdx, %rax # array address + offset into rax
+	# LB Thread 2
+#	movl x(%rip), %ebx # Copy global x to ebx 
+#	movl x, %ebx
+#	movl -4(%rbp), %ecx # Copy counter to ecx
+#	movl %ecx, y(%rip) # write counter to global y
+#	movl %ecx, y
+#	movl %ecx, (%rax)
+//	movl %ebx, (%rax) # write value read to array
+	addl $1, -4(%rbp) # Increment loop counter
+.L2:
+	movq -24(%rbp), %rax
+	movl 24(%rax), %eax
+	cmpl %eax, -4(%rbp) 
+	jle .L3
+	popq %rbp
+	ret

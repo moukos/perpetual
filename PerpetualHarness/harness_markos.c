f5fdf487e9469c25fb1db129b2be439cffe02182
Markos Markakis <markakis@princeton.edu>
2019-04-19 02:38:35 -0400

    Try barriers

--- PerpetualHarness/harness_markos.c
+++ PerpetualHarness/harness_markos.c
@@ -6,6 +6,7 @@
 
 pthread_mutex_t join_mut = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_barrier_t barrier;
 
 typedef struct {
   int *x;
@@ -15,6 +16,9 @@ typedef struct {
   int num_threads;
 } args;
 
+inline static void mbar(void) {
+  asm __volatile__ ("mfence" ::: "memory");
+}
 
 // Must compile with -lpthread and -fopenmp
 extern void P1(void * address);
@@ -25,14 +29,20 @@ void* P1_wrap(void * address) {
   pthread_mutex_lock(&join_mut);
   pthread_cond_wait(&cond, &join_mut);
   pthread_mutex_unlock(&join_mut); 
+  mbar();
+  pthread_barrier_wait(&barrier);
   P1(address);
+  mbar();
 }
 
 void* P2_wrap(void * address) {
   pthread_mutex_lock(&join_mut);
   pthread_cond_wait(&cond, &join_mut);
-  pthread_mutex_unlock(&join_mut); 
+  pthread_mutex_unlock(&join_mut);
+  mbar();
+  pthread_barrier_wait(&barrier);
   P2(address);
+  mbar();
 }
 
 
@@ -72,13 +82,16 @@ int main(int argc,char *argv[]) {
     pthread_t thread1;
     pthread_t thread2;
     printf("Global vars\nThread1\t x: %d, y: %d\nThread2\t x: %d, y: %d\n",*arg_t1.x,*arg_t1.y,*arg_t2.x,*arg_t2.y);
-   
+
+    pthread_barrier_init(&barrier, NULL, 3);
     pthread_create(&thread1, NULL, P1_wrap, (void*)&arg_t1);
     pthread_create(&thread2, NULL, P2_wrap, (void*)&arg_t2);
     sleep(1);
     pthread_cond_broadcast(&cond);
+    pthread_barrier_wait(&barrier);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+    pthread_barrier_destroy(&barrier);
     
     for(i = 0; i < n; i++){
       printf("a: %d b: %d\n", arg_t1.buf[i], arg_t2.buf[i]);

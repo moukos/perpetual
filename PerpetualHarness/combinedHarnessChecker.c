54909a63d8329ffcdbd2c53aff7a2048f8a5f3b7
Themis Melissaris <melissth@gmail.com>
2019-06-06 03:27:09 -0400

    End to end SB harness and checker

--- PerpetualHarness/combinedHarnessChecker.c
+++ PerpetualHarness/combinedHarnessChecker.c
@@ -100,31 +100,54 @@ int main(int argc,char *argv[]) {
     
     for( i = 0; i < n; i++){
  	add_kv1( i+1, arg_t1.buf[i]);
-  //   item1.key = i+1;
-  //    item1.value = arg_t1.buf[i];
-  //    HASH_ADD_INT(h1, i+1, item1);
   	add_kv2( i+1, arg_t2.buf[i]);
-  //    item2.key = i+1;
-  //    item2.value = arg_t2.buf[i];
-  //    HASH_ADD_INT(h2, i+1, item2);
-      printf("Iteration %d \t Th1: %d Th2: %d\n", i, arg_t1.buf[i], arg_t2.buf[i]);
-      fflush(stdout);
+      //printf("Iteration %d \t Th1: %d Th2: %d\n", i, arg_t1.buf[i], arg_t2.buf[i]);
+      //fflush(stdout);
     }
+    int SBinterleavingsCnt = 0;
+    int LBinterleavingsCnt = 0;
+    struct my_struct *t1, *t2, *t3;
+    t1 = malloc(sizeof(struct my_struct));
+    t2 = malloc(sizeof(struct my_struct));
+    t3 = malloc(sizeof(struct my_struct));
 
     for( i = 0; i < n; i++){
-	struct my_struct *s;
 	int temp = i + 1;
-   	HASH_FIND_INT( item1, &temp, s);
-	printf("Id %d, value %d\n",i+1,s->value);
+   	HASH_FIND_INT( item1, &temp, t1);
+	int valueplus = t1->value + 1;
+	int valueminus = t1->value - 1;
+	if( valueplus > n ){
+	  valueplus = n;
+	}
+	if( valueminus < 0 ){
+	  valueminus = 0;
+	}
+	HASH_FIND_INT( item2, &valueplus, t2);
+	//printf("%d %d \n", valueplus, t2->value);
+	//HASH_FIND_INT( item2, &valueminus, t3);
+	//printf("%d %d\n", valueminus, t3->value);
+	
+	// SB Check
+	if ( t2->value == temp-1 ){
+	  SBinterleavingsCnt += 1;
+	  printf("1: key:%d value:%d, 2: key:%d value:%d\n",temp-1,t1->value,valueplus,t2->value);
+	}
+
+	// LB Check
+	//if ( t3->value == temp+1 ){
+	//  LBinterleavingsCnt += 1;
+	//}
+	//printf("1: key:%d value:%d, 2: key:%d value:%d\n",temp-1,t1->value,valueplus,t2->value);
     }
 
-    for( i = 0; i < n; i++){
+/*    for( i = 0; i < n; i++){
 	struct my_struct *s;
 	int temp = i + 1;
    	HASH_FIND_INT( item2, &temp, s);
 	printf("Id %d, value %d\n",i+1,s->value);
     }
-
+*/	
+	printf("SB weak orderings %d \n",SBinterleavingsCnt);
    // printf("\nGlobal vars\nThread1\t x: %d, y: %d\nThread2\t x: %d, y: %d\n",*arg_t1.x,*arg_t1.y,*arg_t2.x,*arg_t2.y);
   
     //free((void *)arg_t1.buf);

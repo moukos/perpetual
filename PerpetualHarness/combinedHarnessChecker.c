dab6b12e915dee9da4f7958270c9f08b3527d636
mmarkakis <markakis@princeton.edu>
2019-09-23 14:56:57 -0400

    Integrated Checker

--- PerpetualHarness/combinedHarnessChecker.c
+++ PerpetualHarness/combinedHarnessChecker.c
@@ -63,7 +63,7 @@ void add_kv2( int key, int value) {
 
 
 int main(int argc,char *argv[]) {
-
+    // Setup
     int i=0;
 
     if(argc < 2){
@@ -89,7 +89,9 @@ int main(int argc,char *argv[]) {
     arg_t1.buf = (volatile int*) calloc(n, sizeof(volatile int));
     void* spacing2 = malloc(40000);
     arg_t2.buf = (volatile int*) calloc(n, sizeof(volatile int));
- //   printf("Global vars\nThread1\t x: %d, y: %d\nThread2\t x: %d, y: %d\n\n",*arg_t1.x,*arg_t1.y,*arg_t2.x,*arg_t2.y);
+
+    // Harness
+    clock_t begin_harness = clock();
 
     omp_set_num_threads(2);
     #pragma omp parallel
@@ -97,62 +99,91 @@ int main(int argc,char *argv[]) {
       if(omp_get_thread_num()==0) P1_wrap((void*)&arg_t1);
       else if (omp_get_thread_num() ==1) P2_wrap((void*)&arg_t2);
     }
+
+    clock_t end_harness = clock();
+    double time_harness = (double) (end_harness - begin_harness) / CLOCKS_PER_SEC;
+    printf("Harness time spent %f \n",  time_harness);
+
+
+    
+    int SBinterleavingsCnt = 0;
+
+    // Checker - Base
+    clock_t begin_base = clock();
+
+    for(i = 0; i < n; i++) 
+      if(arg_t2.buf[arg_t1.buf[i]] < i + 1) SBinterleavingsCnt++;
+
+    clock_t end_base = clock();
+    double time_base = (double) (end_base - begin_base) / CLOCKS_PER_SEC;
+    printf("(Base) SB weak orderings %d \n",SBinterleavingsCnt);
+    printf("(Base) Checker time spent %f \n",  time_base);
+
+    
+    SBinterleavingsCnt = 0;
+    //Checker - Multithreaded
+    clock_t begin_multi = clock();
+
+    int max_threads = omp_get_max_threads();
+    int block_size = n / max_threads;
+    int* counts = (int*) calloc(max_threads, sizeof(int));
+
+    omp_set_num_threads(max_threads);    
+    #pragma omp parallel
+    // for(i = 0; i < n; i++) 
+    // if(arg_t2.buf[arg_t1.buf[i]] < i + 1) SBinterleavingsCnt++;
+    {
+      int thread_num = omp_get_thread_num();
+      int j = thread_num * block_size;
+      int max = j + block_size;
+      int count = 0;
+      
+      for(; j < max; j++) 
+	if(arg_t2.buf[arg_t1.buf[j]] < j + 1) count++;
+
+      counts[thread_num] = count;
+    }
+
+    for (i = 0; i < max_threads; i++) SBinterleavingsCnt += counts[i];
+    
+    clock_t end_multi = clock();
+    double time_multi = (double) (end_multi - begin_multi) / CLOCKS_PER_SEC;
+    printf("(Multi) SB weak orderings %d \n",SBinterleavingsCnt);
+    printf("(Multi) Checker time spent %f \n",  time_multi);
+
+
+    
+    SBinterleavingsCnt = 0;
+    // Checker - Hash table
+    /*    clock_t begin_hash = clock();
     
     for( i = 0; i < n; i++){
  	add_kv1( i+1, arg_t1.buf[i]);
   	add_kv2( i+1, arg_t2.buf[i]);
-      //printf("Iteration %d \t Th1: %d Th2: %d\n", i, arg_t1.buf[i], arg_t2.buf[i]);
-      //fflush(stdout);
     }
-    int SBinterleavingsCnt = 0;
-    int LBinterleavingsCnt = 0;
-    struct my_struct *t1, *t2, *t3;
+    struct my_struct *t1, *t2;
     t1 = malloc(sizeof(struct my_struct));
     t2 = malloc(sizeof(struct my_struct));
-    t3 = malloc(sizeof(struct my_struct));
-
-    for( i = 0; i < n; i++){
-	int temp = i + 1;
-   	HASH_FIND_INT( item1, &temp, t1);
-	int valueplus = t1->value + 1;
-	int valueminus = t1->value - 1;
-	if( valueplus > n ){
-	  valueplus = n;
-	}
-	if( valueminus < 0 ){
-	  valueminus = 0;
-	}
+ 
+    for( i = 1; i <= n; i++){
+      
+   	HASH_FIND_INT( item1, &i, t1);
+	int valueplus = t1->value + 1;	
+	if(valueplus > n) valueplus = n;	
 	HASH_FIND_INT( item2, &valueplus, t2);
-	//printf("%d %d \n", valueplus, t2->value);
-	//HASH_FIND_INT( item2, &valueminus, t3);
-	//printf("%d %d\n", valueminus, t3->value);
 	
 	// SB Check
-	if ( t2->value == temp-1 ){
-	  SBinterleavingsCnt += 1;
-	  printf("1: key:%d value:%d, 2: key:%d value:%d\n",temp-1,t1->value,valueplus,t2->value);
+	if ( t2->value < i ){
+	  SBinterleavingsCnt++;
+	  //  printf("1: key:%d value:%d, 2: key:%d value:%d\n",i,t1->value,valueplus,t2->value);
 	}
-
-	// LB Check
-	//if ( t3->value == temp+1 ){
-	//  LBinterleavingsCnt += 1;
-	//}
-	//printf("1: key:%d value:%d, 2: key:%d value:%d\n",temp-1,t1->value,valueplus,t2->value);
     }
 
-/*    for( i = 0; i < n; i++){
-	struct my_struct *s;
-	int temp = i + 1;
-   	HASH_FIND_INT( item2, &temp, s);
-	printf("Id %d, value %d\n",i+1,s->value);
-    }
-*/	
-	printf("SB weak orderings %d \n",SBinterleavingsCnt);
-   // printf("\nGlobal vars\nThread1\t x: %d, y: %d\nThread2\t x: %d, y: %d\n",*arg_t1.x,*arg_t1.y,*arg_t2.x,*arg_t2.y);
-  
-    //free((void *)arg_t1.buf);
-    //free((void *)arg_t2.buf);
-    
+    clock_t end_hash = clock();
+    double time_hash = (double) (end_hash - begin_hash) / CLOCKS_PER_SEC;
+    printf("(Hash) SB weak orderings %d \n",SBinterleavingsCnt);
+    printf("(Hash) Checker time spent %f \n",  time_hash);
+    */
     return 0;
 }
 

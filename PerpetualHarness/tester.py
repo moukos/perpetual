0168d1b7e1fa6883d7efe96e740b19fa0ddffaf5
mmarkakis <markakis@princeton.edu>
2019-09-27 15:45:39 -0400

    tester for both perple/litmus

--- PerpetualHarness/tester.py
+++ PerpetualHarness/tester.py
@@ -5,34 +5,40 @@ import datetime
 import numpy
 
 
-# Parse command line arguments
-start = numpy.int64(sys.argv[1])
-steps = numpy.int64(sys.argv[2])
-tries = numpy.int64(sys.argv[3])
+## Parse command line arguments
+testname = sys.argv[1]
+start = numpy.int64(sys.argv[2])
+steps = numpy.int64(sys.argv[3])
+tries = numpy.int64(sys.argv[4])
 
-# Set up file
+## Set up files
+# Compile executable
+execfilename = "integratedHarness_" + testname
+threadfilenames = testname + "_thread_*"
+subprocess.call("gcc -fopenmp -o " + execfilename + " integratedHarness.c " + threadfilenames, shell=True)
+# Open output file
 now = datetime.datetime.now()
-outfilename = now.strftime("%Y-%m-%d_%H:%M:%S")
-outfilename = "output_" + outfilename
+outfilename = "output_" + testname + "_" + now.strftime("%Y-%m-%d_%H:%M:%S")
 f = open(outfilename, 'w+')
 
-# Run tests and write file
+## Run tests with PerpLE and write file
 n = start
 for j in range(steps):
     for i in range(tries):
-        subprocess.call(["./combinedHarnessChecker", str(n)], stdout=f)
+        subprocess.call(["./" + execfilename, str(n)], stdout=f)
     n = 10 * n
 f.close()
     
-# Analyze file and print stats
-print("Summary of results (start = %d, steps = %d, tries = %d)\n" % (start, steps, tries))
+## Analyze file and print stats
+print("\nSummary of PerpLE results (start = %d, steps = %d, tries = %d)\n" % (start, steps, tries))
+print("Iterations    |Weak Outcomes |% Weak   |Harness Time  |Checker Time")
 g = open(outfilename, 'r')
 linecounter = 0
 weaksum = 0
 harnesssum = 0
 checkersum = 0
 m = start
-formatstring = "%d Iterations: \t\t\t%d Weak behaviors (%4.2f/100) \t\t\tHarness time: %4.2f \tChecker time: %4.2f\n"             
+formatstring = "%-14d|%-14d|%-9.2f|%-14.2f|%-14.2f"             
 for line in g:
     linecounter += 1
     if (linecounter % tries == 0):
@@ -46,4 +52,48 @@ for line in g:
         weaksum += numpy.int64(fields[1])
         harnesssum += float(fields[2])
         checkersum += float(fields[3])
+print("\n")
 g.close()
+
+
+# Open output file
+now = datetime.datetime.now()
+outfilename = "output_" + testname + "_" + now.strftime("%Y-%m-%d_%H:%M:%S")
+f = open(outfilename, 'w+')
+
+## Run tests with litmus and write file
+n = start
+for j in range(steps):
+    subprocess.call(["litmus", testname + ".litmus", "-r", str(tries), "-s", str(n)], stdout=f)
+    n = 10 * n
+f.close()
+
+## Analyze file and print stats
+print("\nSummary of litmus results (start = %d, steps = %d, tries = %d)\n" % (start, steps, tries))
+print("Iterations    |Weak Outcomes |% Weak   |Litmus Time")
+g = open(outfilename, 'r')
+pointer = 0
+weak = 0
+time = 0
+m = start
+formatstring = "%-14d|%-14d|%-9.2f|%-14.2f"             
+for line in g:
+    if(line.find("Observation " + testname) != -1):
+        weakstart = line.find(" ", line.find(" ", line.find(" ") + 1 ) + 1) + 1
+        weakend = line.find(" ", weakstart)
+        weak = numpy.int64(line[weakstart:weakend])
+    if(line.find("Time " + testname) != -1):
+        timestart = line.find(" ", line.find(" ") + 1) + 1
+        timeend = line.find(" ", timestart)
+        time = numpy.float64(line[timestart:timeend])
+        print(formatstring % (m, weak/tries, 100*weak/tries/m, time/tries))
+        m = 10 * m
+        weak = 0
+        time = 0
+   
+print("\n")
+g.close()
+
+
+## Clean up
+subprocess.call("rm output_*",shell = True )

a1736afe72e4160b8f06a92cba1554f9cab530f1
mmarkakis <markakis@princeton.edu>
2019-10-31 15:17:44 -0400

    cleanup v2

--- __PerpLE/compile.sh
+++
@@ -1,9 +0,0 @@
-#!usr/bin/bash
-
-echo Which test to compile?
-
-read testname
-
-gcc -fopenmp -o $testname"_harness" harness.c $testname/$testname"_0.s" $testname/$testname"_1.s" $testname/$testname"_2.s" $testname/$testname"_3.s"
-
-echo done!

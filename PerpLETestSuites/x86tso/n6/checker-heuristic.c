f8c9fd33baea3fe77efa738ad5446bfc14329d49
mmarkakis <markakis@princeton.edu>
2019-11-04 09:21:23 -0500

    Created handcheck directory

--- PerpLETestSuites/x86tso/n6/checker-heuristic.c
+++
@@ -1,9 +0,0 @@
-long condition2(volatile long *buf0, volatile long *buf1, volatile long *buf2, volatile long *buf3, long N){
-	long n=0, sum=0;
-	long numberUp = 0;
-	for(n = 0; n < N; n++ ){ 
-		if(buf0[2 * n + 0] < buf0[2 * n + 0] + 2)
-			sum++;
-	}
-	return sum;
-}
--- PerpLETestSuites/x86tso/safe033/safe033_thread_3.s
+++
@@ -1,6 +0,0 @@
-	.section ".text"
-	.globl P3
-	.type P3, @function
-
-P3:
-	ret

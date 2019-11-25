caf1de94e03e8af80937cb5dc14972366159ade2
mmarkakis <markakis@princeton.edu>
2019-11-25 10:31:07 -0500

    n4 fixed

--- PerpLETestSuitesChecked/x86tso/n4/checker.c
+++ PerpLETestSuitesChecked/x86tso/n4/checker.c
@@ -6,11 +6,11 @@ long condition(volatile long *buf0, volatile long *buf1, volatile long *buf2, vo
 			long leftEdgeEnd1 = buf0[2 * n + 0];
 			long leftEdgeEnd2 = buf0[2 * n + 1];
 			if((leftEdgeEnd1 >= 2 * m + 2) && (leftEdgeEnd1 % 2 == 0)&&
-			   (leftEdgeEnd2 < 2 * n + 2) && (leftEdgeEnd2 % 2 == 1)){
+			   (leftEdgeEnd2  == 2 * n + 1)){
 				long rightEdgeEnd1 = buf1[2 * m + 0];
 				long rightEdgeEnd2 = buf1[2 * m + 1];
-				if((rightEdgeEnd1 < 2 * n + 2) && (rightEdgeEnd1 % 2 == 1)&&
-			           (rightEdgeEnd2 >= 2 * m + 2) && (rightEdgeEnd2 % 2 == 0A))
+				if((rightEdgeEnd1 >= 2 * n + 1) && (rightEdgeEnd1 % 2 == 1)&&
+			           (rightEdgeEnd2 == 2 * m + 2))
 				  sum++;
 			
 			}

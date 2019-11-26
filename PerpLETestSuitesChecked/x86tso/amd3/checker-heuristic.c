94501c9f9696a9b3210170b867f601972bca30f6
mmarkakis <markakis@princeton.edu>
2019-11-25 22:34:49 -0500

    amd3 fixes

--- PerpLETestSuitesChecked/x86tso/amd3/checker-heuristic.c
+++ PerpLETestSuitesChecked/x86tso/amd3/checker-heuristic.c
@@ -2,7 +2,7 @@ long condition2(volatile long *buf0, volatile long *buf1, volatile long *buf2, v
 	long n=0, sum=0;
 	long numberUp = 0;
 	for(n = 0; n < N; n++ ){ 
-	  if((buf1[(buf0[1 * n + 0] - 1) / 2] < 2 * n + 2) && (buf1[(buf0[n]-1)/2] >= 2*n+1) && (buf0[n] % 2 == 1) && ((buf0[1 * n + 0] - 1) / 2 < N) && ((buf0[1*n] -1) / 2 > 0))
+	  if((buf1[(buf0[1 * n + 0] - 1) / 2] < 2 * n + 2) && ((buf0[1 * n + 0] - 1) / 2 < N) && ((buf0[1*n] -1) / 2 > 0))
 			sum++;
 	}
 	return sum;
